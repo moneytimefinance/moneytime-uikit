f72ecf96d44641f65fad1813c0526e72303b673b
dmitry <dmitry>
2021-06-30 07:28:14 -0700

    update build

---
+++ dist/components/Checkbox/types.d.ts
@@ -0,0 +1,9 @@
+export declare const scales: {
+    readonly SM: "sm";
+    readonly MD: "md";
+    readonly LG: "lg";
+};
+export declare type Scales = typeof scales[keyof typeof scales];
+export interface CheckboxProps {
+    scale?: Scales;
+}
